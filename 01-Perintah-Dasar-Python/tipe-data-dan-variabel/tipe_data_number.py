a4a9ae94514cf1f5f6f888a2d41c6cc45ce66c66
Ayo Belajar <166281362+70korikoSolusi@users.noreply.github.com>
2025-09-16 21:21:11 +0700

    Update and rename tipe_data_primitive.py to tipe_data_number.py

---
+++ 01-Perintah-Dasar-Python/tipe-data-dan-variabel/tipe_data_number.py
@@ -0,0 +1,29 @@
+# tipe data number 
+panjang = 5
+lebar = 10.5
+luas = panjang * lebar
+print(panjang, '*', lebar, '=', luas)
+print("Tipe dari variabel panjang:", type(panjang))
+print("Tipe dari variabel lebar:", type(lebar))
+print("Tipe dari variabel luas:", type(luas))
+print()
+a = 5j
+b = 10j
+c = a + b
+print(a, '+', b, '=', c)
+print('Tipe dari a:', type(a))
+print('Tipe dari b:', type(b))
+print('Tipe dari c:', type(c))
+# tipe data string
+nama_depan = "Wahit"
+nama_belakang = 'Abdulloh'
+nama_lengkap = nama_depan + ' ' + nama_belakang
+usia = '12'
+alamat = 'Bangkalan'
+kata_mutiara = "Don't judge a book by it's cover"
+print(nama_lengkap, '(' + usia + ')', ',', 'dari', alamat, ', kata mutiara:', kata_mutiara)
+print('\nTipe dari nama_lengkap:', type(nama_lengkap))
+print('Tipe dari usia:', type(usia))
+print('Tipe dari alamat:', type(alamat))
+print('Tipe dari kata_mutiara:', type(kata_mutiara))
+
