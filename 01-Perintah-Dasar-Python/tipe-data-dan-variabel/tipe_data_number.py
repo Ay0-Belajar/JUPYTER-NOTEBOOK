d4bb7af11988afbc921c2fdda7e9a25d0ac7dabd
Ayo Belajar <166281362+70korikoSolusi@users.noreply.github.com>
2025-09-16 21:21:47 +0700

    Update tipe_data_number.py

--- 01-Perintah-Dasar-Python/tipe-data-dan-variabel/tipe_data_number.py
+++ 01-Perintah-Dasar-Python/tipe-data-dan-variabel/tipe_data_number.py
@@ -14,16 +14,5 @@ print(a, '+', b, '=', c)
 print('Tipe dari a:', type(a))
 print('Tipe dari b:', type(b))
 print('Tipe dari c:', type(c))
-# tipe data string
-nama_depan = "Wahit"
-nama_belakang = 'Abdulloh'
-nama_lengkap = nama_depan + ' ' + nama_belakang
-usia = '12'
-alamat = 'Bangkalan'
-kata_mutiara = "Don't judge a book by it's cover"
-print(nama_lengkap, '(' + usia + ')', ',', 'dari', alamat, ', kata mutiara:', kata_mutiara)
-print('\nTipe dari nama_lengkap:', type(nama_lengkap))
-print('Tipe dari usia:', type(usia))
-print('Tipe dari alamat:', type(alamat))
-print('Tipe dari kata_mutiara:', type(kata_mutiara))
+
 
